0b1a21e90c53498821c77e55f70349d122230e8b
yogapriya1995 <89956656+yogapriya1995@users.noreply.github.com>
2021-09-02 10:00:28 +0530

    First commit

---
+++ demo/src/main/java/com/example/demo/Repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.Repository;
+import org.springframework.data.repository.CrudRepository;  
+import com.example.demo.model.Student;  
+
+
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+
+}
